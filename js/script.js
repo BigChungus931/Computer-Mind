21ed408a7ccfca4021e2f4d5ba96d95f1253d943
BigChungus931 <borsoshkin@gmail.com>
2025-06-14 12:57:21 -0600

    Made and added acc section to 2 sections

--- js/script.js
+++ js/script.js
@@ -2,3 +2,21 @@ function hamburgerf() {
   const sidebar = document.getElementById("sidebar");
   sidebar.classList.toggle("active");
 }
+const headers = document.querySelectorAll(".acc-header");
+headers.forEach((header) => {
+  header.addEventListener("click", () => {
+    const content = header.nextElementSibling;
+
+    document.querySelectorAll(".acc-content").forEach((c) => {
+      if (c !== content) {
+        c.style.maxHeight = null;
+      }
+    });
+
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+});
